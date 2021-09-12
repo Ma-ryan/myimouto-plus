Ma-ryan/myimouto-plus

commit f8277a34412871a8af3fefb7fcc0771ba7173ae0

Fix XSS vulnerabilities due to malicious user names

app/views/user/home.php
@@ -13,7 +13,7 @@
       <li><?= $this->linkTo($this->t('.reset_password'), ['action' => 'reset_password']) ?></li>
     </ul>
   <?php else: ?>
-    <h2><?= $this->t(['.greet_user', 'u' => current_user()->name]) ?></h2>
+    <h2><?= $this->t(['.greet_user', 'u' => $this->h(current_user()->name])) ?></h2>
     <p><?= $this->t('.action_info') ?></p>
 
     <div class="section">

app/views/comment/_comment.php
@@ -1,9 +1,9 @@
 <div class="comment avatar-container" id="c<?= $this->comment->id ?>">
   <div class="author">
     <?php if ($this->comment->user_id) : ?>
-      <h6><?= $this->linkTo($this->comment->pretty_author2(), array('controller' => 'user', 'action' => 'show', 'id' => $this->comment->user_id)) ?></h6>
+      <h6><?= $this->linkTo($this->h($this->comment->pretty_author2()), array('controller' => 'user', 'action' => 'show', 'id' => $this->comment->user_id)) ?></h6>
     <?php else: ?>
-      <h6><?= $this->comment->pretty_author2() ?></h6>
+      <h6><?= $this->h($this->comment->pretty_author2()) ?></h6>
     <?php endif ?>
     <span class="date" title="Posted at <?= date('r', strtotime($this->comment->created_at)) ?>">
       <?= $this->linkTo($this->t(array('time.x_ago', 't' => $this->timeAgoInWords($this->comment->created_at))), array('post#show', 'id' => $this->comment->post_id, 'anchor' => "c".$this->comment->id)) ?>

app/views/history/index.php
@@ -76,7 +76,7 @@
               ) ?></td>
             <?php endif ?>
             <td><?= date("M d Y, H:i", strtotime($change->created_at)) ?></td>
-            <td class="author"><?= $this->linkToIf($change->user_id, $change->author(),
+            <td class="author"><?= $this->linkToIf($change->user_id, $this->h($change->author()),
               ['controller' => "user", 'action' => "show", 'id' => $change->user_id],
               ['class' => "user-" . $change->user_id . ($new_user ? " new-user":"")]
             ) ?></td>

app/views/post/show_partials/_statistics_panel.php
@@ -2,7 +2,7 @@
   <h5><?= $this->t('.title') ?></h5>
   <ul>
     <li><?= $this->t('.id') ?>: <?= $this->post->id ?></li>
-    <li><?= $this->t('.posted') ?>: <?= $this->t(['.posted_data_html', 'time' => $this->linkTo($this->t(['time.x_ago', 't' => $this->timeAgoInWords($this->post->created_at)]), ["#index", 'tags' => "date:" . substr($this->post->created_at, 0, 10)], ['title' => substr(date('r', strtotime($this->post->created_at)), 0, -6)]), 'user' => $this->linkToIf($this->post->user_id, $this->post->author(), ['user#show', 'id' => $this->post->user_id])]) ?></li>
+    <li><?= $this->t('.posted') ?>: <?= $this->t(['.posted_data_html', 'time' => $this->linkTo($this->t(['time.x_ago', 't' => $this->timeAgoInWords($this->post->created_at)]), ["#index", 'tags' => "date:" . substr($this->post->created_at, 0, 10)], ['title' => substr(date('r', strtotime($this->post->created_at)), 0, -6)]), 'user' => $this->linkToIf($this->post->user_id, $this->h($this->post->author()), ['user#show', 'id' => $this->post->user_id])]) ?></li>
     <?php if (current_user()->is_admin() && $this->post->approver) : ?>
       <li><?= $this->t('.approver') ?>: <?= $this->post->approver->name ?></li>
     <?php endif ?>

commit 1ac586f9fb97975a0d3d4281b542423e13be6d44

Fix what I broke in commit f8277a3

app/views/user/home.php
@@ -13,7 +13,7 @@
       <li><?= $this->linkTo($this->t('.reset_password'), ['action' => 'reset_password']) ?></li>
     </ul>
   <?php else: ?>
-    <h2><?= $this->t(['.greet_user', 'u' => $this->h(current_user()->name])) ?></h2>
+    <h2><?= $this->t(['.greet_user', 'u' => $this->h(current_user()->name)]) ?></h2>
     <p><?= $this->t('.action_info') ?></p>
 
     <div class="section">