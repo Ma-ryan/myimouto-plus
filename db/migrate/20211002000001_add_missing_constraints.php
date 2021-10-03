fcfa94ba84a2d8a5dcc49038991953c7eaa7fe62
vesta42 <vesta42+gh@pm.me>
2021-10-02 22:39:23 -0500

    add checks to migration before creating constraints

--- db/migrate/20211002000001_add_missing_constraints.php
+++ db/migrate/20211002000001_add_missing_constraints.php
@@ -6,21 +6,21 @@ class AddMissingConstraints extends Rails\ActiveRecord\Migration\Base
     public function up()
     {
         // add missing foreign key constraints
-        $this->createForeignKey('favorites', 'post_id', 'posts', 'id', ['delete' => 'cascade']);
-        $this->createForeignKey('favorites', 'user_id', 'users', 'id');
-        $this->createForeignKey('histories', 'user_id', 'users', 'id');
-        $this->createForeignKey('note_versions', 'user_id', 'users', 'id');
-        $this->createForeignKey('notes', 'user_id', 'users', 'id');
-        $this->createForeignKey('pools', 'user_id', 'users', 'id');
-        $this->createForeignKey('post_tag_histories', 'user_id', 'users', 'id');
-        $this->createForeignKey('wiki_page_versions', 'user_id', 'users', 'id');
+        $this->createForeignKey('Favorite', 'favorites', 'post_id', 'posts', 'id', ['delete' => 'cascade']);
+        $this->createForeignKey('Favorite', 'favorites', 'user_id', 'users', 'id');
+        $this->createForeignKey('History', 'histories', 'user_id', 'users', 'id');
+        $this->createForeignKey('NoteVersion', 'note_versions', 'user_id', 'users', 'id');
+        $this->createForeignKey('Note', 'notes', 'user_id', 'users', 'id');
+        $this->createForeignKey('Pool', 'pools', 'user_id', 'users', 'id');
+        $this->createForeignKey('PostTagHistory', 'post_tag_histories', 'user_id', 'users', 'id');
+        $this->createForeignKey('WikiPageVersion', 'wiki_page_versions', 'user_id', 'users', 'id');
 
         // there is a duplicate foreign key on tag_implications.consequent_id so remove the duplicate
         $this->dropForeignKey('tag_implications', 'fk_consequent_id');
 
         // add missing unique key constraints
-        $this->createUniqueKey('artists', 'name');
-        $this->createUniqueKey('wiki_pages', 'title');
+        $this->createUniqueKey('Artist', 'artists', 'name');
+        $this->createUniqueKey('WikiPage', 'wiki_pages', 'title');
 
         // add some more indexes to help improve performance
         $this->createIndex('histories', ['group_by_id', 'group_by_table']); // improves object specific history performance
@@ -40,6 +40,33 @@ class AddMissingConstraints extends Rails\ActiveRecord\Migration\Base
     }
 
 
+    private function checkUniqueKeyConflicts($model, $table, $attr)
+    {
+        $conflicts = 0;
+        $sql = "SELECT * FROM `{$table}` GROUP BY `{$attr}` HAVING COUNT(`{$attr}`) > 1";
+        $collection = call_user_func([$model, 'findBySql'], $sql);
+        foreach ($collection as $obj) { ++$conflicts; }
+        if ($conflicts > 0) { throw new \Exception(
+            "Cannot create unique key for {$table}.{$attr};"
+            . " {$conflicts} set" . ($conflicts>1?'s':'') . " of duplicates exists"); }
+    }
+
+
+
+    private function checkForeignKeyConflicts($model, $table, $column, $reftable, $refcolumn)
+    {
+        $conflicts = 0;
+        $sql = "SELECT `{$table}`.* FROM `{$table}` LEFT JOIN `{$reftable}`"
+            . " ON `{$table}`.`{$column}` = `{$reftable}`.`${refcolumn}`"
+            . " WHERE `{$reftable}`.`{$refcolumn}` IS NULL";
+        $collection = call_user_func([$model, 'findBySql'], $sql);
+        foreach ($collection as $obj) { ++$conflicts; }
+        if ($conflicts > 0) { throw new \Exception(
+            "Cannot create foreign key for {$table}.{$column} => {$reftable}.{$refcolumn};"
+            . " {$conflicts} rows exists in the left table that reference non-existant rows in the right table"); }
+    }
+
+
 
     private function dropForeignKey($table, $name)
     {
@@ -50,8 +77,9 @@ class AddMissingConstraints extends Rails\ActiveRecord\Migration\Base
 
 
 
-    private function createUniqueKey($table, $column)
+    private function createUniqueKey($model, $table, $column)
     {
+        $this->checkUniqueKeyConflicts($model, $table, $column);
         $name = 'uk_' . $table . '__' . $column;
         $this->execute("IF NOT EXISTS (SELECT 1 FROM `information_schema`.`TABLE_CONSTRAINTS`"
             . " WHERE `CONSTRAINT_SCHEMA`=DATABASE() AND `TABLE_NAME`='{$table}' AND `CONSTRAINT_NAME`='{$name}')"
@@ -59,8 +87,9 @@ class AddMissingConstraints extends Rails\ActiveRecord\Migration\Base
     }
 
 
-    private function createForeignKey($table, $column, $reftable, $refcolumn, $options = [])
+    private function createForeignKey($model, $table, $column, $reftable, $refcolumn, $options = [])
     {
+        $this->checkForeignKeyConflicts($model, $table, $column, $reftable, $refcolumn);
         $name = 'fk_' . $table . '__' . $column;
         $this->execute("IF NOT EXISTS (SELECT 1 FROM `information_schema`.`TABLE_CONSTRAINTS`"
             . " WHERE `CONSTRAINT_SCHEMA`=DATABASE() AND `TABLE_NAME`='{$table}' AND `CONSTRAINT_NAME`='{$name}')"
