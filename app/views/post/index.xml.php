260b0219ad9aa6cf84f955274177ac20908342e5
vesta42 <vesta42+gh@pm.me>
2021-10-25 18:42:57 -0500

    [API] fix invalid XML in post/index

--- app/views/post/index.xml.php
+++ app/views/post/index.xml.php
@@ -1,10 +1,18 @@
 <?php
-echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
-echo $this->contentTag('posts', function() {
-    // Make it look pretty.
-    echo "\n";
-    
-    foreach ($this->posts as $post) {
-        echo '  ' . $this->tag('post', $post->api_attributes(), false, true) . "\n";
+$dom = new DOMDocument('1.0', 'UTF-8');
+$root = $dom->appendChild($dom->createElement('posts'));
+$root->setAttribute('count', $this->posts->totalRows());
+$root->setAttribute('offset', ($this->posts->currentPage() - 1) * $this->posts->perPage());
+
+foreach ($this->posts as $post)
+{
+    $pel = $root->appendChild($dom->createElement('post'));
+
+    foreach ($post->api_attributes() as $key => $value)
+    {
+        if (!$value) { continue; }
+        $pel->setAttribute($key, $value);
     }
-}, ['count' => $this->posts->totalRows(), 'offset' => ($this->posts->currentPage() - 1) * $this->posts->perPage()]);
+}
+
+echo($dom->saveXML());
